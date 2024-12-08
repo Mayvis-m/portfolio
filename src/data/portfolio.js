2c2c0af13611ea1c89e2def74b1ab7f0507e7cc6
Margaret <malv207@wgu.edu>
2024-12-07 22:42:45 -0600

    Added file path

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,21 +1,21 @@
 export default [
     {
         title: 'Texas Website',
-        imgURL: 'portfolio/assets/Texas.png',
+        imgURL: '/portfolio/assets/Texas.png',
         stack: ['HTML', 'CSS', 'JavaScript'],
         link: 'https://mayvis-m.github.io/Texas.github.io/',
     },
 
     {
         title: 'Portfolio',
-        imgURL: 'portfolio/assets/minimal-portfolio.png',
+        imgURL: '/portfolio/assets/minimal-portfolio.png',
         stack: ['React', 'Tailwind', 'Vite'],
         link: '',
     },
 
     {
         title: 'Snake Game',
-        imgURL: 'portfolio/assets/snake.png',
+        imgURL: '/portfolio/assets/snake.png',
         stack: ['Python', 'Tkinter'],
         link: 'https://github.com/Mayvis-m/Snake-Game',
     },
